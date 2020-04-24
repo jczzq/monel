cc1c0f6ad0f36d2228c17dc2177048f68f805135
jczzq <jinchao@tuhu.cn>
2020-04-24 15:27:57 +0800

    chore: config

--- config/rollup.js
+++ config/rollup.js
@@ -4,19 +4,18 @@ var pkg = require('../package.json');
 
 var version = pkg.version;
 
-var banner = 
-`/*!
+var banner = `/*!
  * ${pkg.name} ${version} (https://github.com/jczzq/monel)
  * API https://github.com/jczzq/monel/blob/master/doc/api.md
- * Copyright 2017-${(new Date).getFullYear()} jczzq. All Rights Reserved
+ * Copyright 2017-${new Date().getFullYear()} jczzq. All Rights Reserved
  * Licensed under MIT (https://github.com/jczzq/monel/blob/master/LICENSE)
  */
 `;
 
 function getCompiler(opt) {
     opt = opt || {
-        tsconfigOverride: { compilerOptions : { module: 'ES2015' } }
-    }
+        tsconfigOverride: { compilerOptions: { module: 'ES2015' } },
+    };
 
     return typescript(opt);
 }
